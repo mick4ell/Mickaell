659851383ca7dfe9c07c0934b812a071ba7b492e
mick4ell <mick4ell>
2026-03-27 12:30:26 -0300

    novo

---
+++ curso_em_video/ex07.py
@@ -0,0 +1,4 @@
+n1 = float(input('digite a nota 1:'))
+n2 = float(input('digite a nota 2:'))
+media = (n1+n2)/2
+print('a media é {}'.format(media))
---
+++ curso_em_video/ex08.py
@@ -0,0 +1,4 @@
+med = float(input('digite uma medida em metros: '))
+cm = med*100
+mm = med*1000
+print('a medida é {}m')
