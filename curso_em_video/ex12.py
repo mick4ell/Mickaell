930c3b73437739be2d9cb284ff05ef9f5d0c0ac3
mickaell <mickaell>
2026-04-10 09:17:41 -0300

    novo

---
+++ curso_em_video/ex12.py
@@ -0,0 +1,3 @@
+pre = float(input('Qual é o preço do produto R$'))
+de = pre - (pre * 5 / 100)
+print (f'o produto que custava R${pre:.2f}, na promação de 5% fica R${de:.2f}')
---
+++ unasp_2026/Atividade01.py
@@ -0,0 +1,8 @@
+nome = input('Digite o seu nome: ')
+nivel_acesso = int(input('Qual é o seu nivel de Privilégio: '))
+chave_ativa = input('Você tem a chave de criptografia: ')
+
+perm = acesso_permitido = (nivel_acesso >= 3)) and chave_ativa == True
+
+print(f'Verificando segurança para: {nome}')
+print(f'Acesso autorizado: {perm}')
